a332da20d8d344243685f793a6efe9e259364c59
ryonasso <ryounaoyuki@gmail.com>
2021-08-28 23:31:18 +0900

    feat : 作成したテスト一覧を見る画面の作成

--- src/components/TestView.jsx
+++ src/components/TestView.jsx
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, VStack, Center } from "@chakra-ui/react";
 import { getQuestion } from "../api/questionApi";
 import { Test } from "./Test";
 
 export const TestView = () => {
-  const [tests, setTests] = useState();
+  const [tests, setTests] = useState([]);
+  let result;
   useEffect(() => {
-    const result = getQuestion().then((data) => {
+    getQuestion().then((data) => {
       setTests(data);
     });
   }, []);
 
+  if (tests !== []) {
+    result = 1;
+  }
+
   return (
-    <Box>
-      <Box>テスト一覧画面です</Box>
-      <Test />
-    </Box>
+    <Center>
+      <VStack spacing={8}>
+        {result === null ? (
+          <Box></Box>
+        ) : (
+          tests.map((test) => {
+            return <Test key={test.id} props={test} />;
+          })
+        )}
+      </VStack>
+    </Center>
   );
 };
