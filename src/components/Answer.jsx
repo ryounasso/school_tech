f3e63956f452e133b8d82758923a77b786e3ba9d
ryonasso <ryounaoyuki@gmail.com>
2021-08-29 02:18:14 +0900

    refactor : 不要な変数の削除

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,17 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Box,
-  FormControl,
-  FormLabel,
-  Text,
-  Input,
-  VStack,
-  HStack,
-  Spacer,
-  Button,
-  Heading,
-  Center,
-} from "@chakra-ui/react";
+import { Box, VStack, Heading, Center } from "@chakra-ui/react";
 import { useAuth } from "../contexts/authContext";
 import { getQuestion } from "../api/questionApi";
 import { Question } from "./Question";
@@ -30,54 +18,6 @@ export const Answer = () => {
   }
 
   const { user } = useAuth();
-  const [problem, setProblem] = useState("");
-  const [choiceA, setChoiceA] = useState("");
-  const [choiceB, setChoiceB] = useState("");
-  const [choiceC, setChoiceC] = useState("");
-  const [choiceD, setChoiceD] = useState("");
-  const [result, setResult] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const question = {
-      problem: problem,
-      choiceA: choiceA,
-      choiceB: choiceB,
-      choiceC: choiceC,
-      choiceD: choiceD,
-      result: result,
-    };
-    setProblem("");
-    setChoiceA("");
-    setChoiceB("");
-    setChoiceC("");
-    setChoiceD("");
-    setResult("");
-  };
-
-  const handleChangePloblem = (e) => {
-    setProblem(e.currentTarget.value);
-  };
-
-  const handleChangeChoiceA = (e) => {
-    setChoiceA(e.currentTarget.value);
-  };
-
-  const handleChangeChoiceB = (e) => {
-    setChoiceB(e.currentTarget.value);
-  };
-
-  const handleChangeChoiceC = (e) => {
-    setChoiceC(e.currentTarget.value);
-  };
-
-  const handleChangeChoiceD = (e) => {
-    setChoiceD(e.currentTarget.value);
-  };
-
-  const handleChangeResult = (e) => {
-    setResult(e.currentTarget.value);
-  };
 
   return (
     <>
